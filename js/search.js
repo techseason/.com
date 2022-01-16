d5b8685c31d662db59421debda853e06d14e1d08
Live Aatma <amanchaudharykumar@gmail.com>
2022-01-16 15:16:50 +0530

    mistake

--- js/search.js
+++ js/search.js
@@ -8,7 +8,7 @@ $(function () {
     { value: "Basic Electronics Engg.", link: 'https://drive.google.com/uc?export=download&id=1Qo-WS_1PlqvZdfHMp3MIhuhWvA9hwmOw' },
     { value: "Manufacturing Techniques", link: 'https://drive.google.com/uc?export=download&id=1AnsOhjbQU39V7eVtvdP1_KamT2dwzKEd' },
     { value: "Engineering Graphics", link: 'https://drive.google.com/u/0/uc?export=download&confirm=KCV4&id=1_2O6T7hA0S15bADkARrCHsQvmvsFgfbl' },
-    { value: "Engineering Physics-1 Lab", link: '/u/0/uc?export=download&amp;confirm=44Xg&amp;id=1eKSKaCEUH9ZDp8KaOUvY5Rru465JN1yk' },
+    { value: "Engineering Physics-1 Lab", link: 'https://drive.google.com/uc?export=download&id=1eKSKaCEUH9ZDp8KaOUvY5Rru465JN1yk' },
     { value: "Basic Electronics Engg. Lab", link: 'https://drive.google.com/uc?export=download&id=10rmidecVMel-9lyM6-o4vwnmYJAaKxWr' },
     { value: "Manufacturing Techniques Lab", link: 'https://drive.google.com/uc?export=download&id=1OcKtxmcgWqqvKtx-D_3OWaX3e2IPynJg' },];
   $("#tags").autocomplete({
