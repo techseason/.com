0ec65fcb004a40c3e2ce6ff1178879b78507963c
Live Aatma <amanchaudharykumar@gmail.com>
2022-01-16 11:20:00 +0530

    search

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
 var $ = jQuery;
 $(function () {
   var availableTags = [
-    { value: "Mathematics-1", link: 'https://instagram.com/tech_season' },
-    { value: "Physics-1", link: 'https://google.com' },
-    { value: "Economics", link: 'https://google.com' },
-    { value: "Enviroment Studies", link: 'https://google.com' },
-    { value: "Basic Electronics Engg.", link: 'https://google.com' },
-    { value: "Manufacturing Techniques", link: 'https://google.com' },
-    { value: "Engineering Graphics", link: 'https://google.com' },
-    { value: "Engineering Physics-1 Lab", link: 'https://google.com' },
-    { value: "Basic Electronics Engg. Lab", link: 'https://google.com' },
-    { value: "Manufacturing Techniques Lab", link: 'https://google.com' },];
+    { value: "Mathematics-1", link: 'https://drive.google.com/u/0/uc?export=download&confirm=bCoY&id=1zV3R9VkiRQrWs9LjYCadPeaQOI5iq45w" class=stretched-link' },
+    { value: "Physics-1", link: 'https://drive.google.com/u/0/uc?export=download&confirm=SFYH&id=1eKSKaCEUH9ZDp8KaOUvY5Rru465JN1yk' },
+    { value: "Economics", link: 'https://drive.google.com/uc?export=download&id=1nLZdoXIMiXpzPRswpt_iTkIDsOdVDqpA' },
+    { value: "Enviroment Studies", link: 'https://drive.google.com/uc?export=download&id=1s--Bt3IWGHrjoriNEjkdl7LTc0ju_Kgm' },
+    { value: "Basic Electronics Engg.", link: 'https://drive.google.com/uc?export=download&id=1Qo-WS_1PlqvZdfHMp3MIhuhWvA9hwmOw' },
+    { value: "Manufacturing Techniques", link: 'https://drive.google.com/uc?export=download&id=1AnsOhjbQU39V7eVtvdP1_KamT2dwzKEd' },
+    { value: "Engineering Graphics", link: 'https://drive.google.com/u/0/uc?export=download&confirm=KCV4&id=1_2O6T7hA0S15bADkARrCHsQvmvsFgfbl' },
+    { value: "Engineering Physics-1 Lab", link: '/u/0/uc?export=download&amp;confirm=44Xg&amp;id=1eKSKaCEUH9ZDp8KaOUvY5Rru465JN1yk' },
+    { value: "Basic Electronics Engg. Lab", link: 'https://drive.google.com/uc?export=download&id=10rmidecVMel-9lyM6-o4vwnmYJAaKxWr' },
+    { value: "Manufacturing Techniques Lab", link: 'https://drive.google.com/uc?export=download&id=1OcKtxmcgWqqvKtx-D_3OWaX3e2IPynJg' },];
   $("#tags").autocomplete({
     source: availableTags,
     select: function (event, ui) {
